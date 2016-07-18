1ef171b399b576e8d4396ff5289ec80735e904d8
Bruno Randolf <br1@einfach.org>
2016-07-18 16:29:25 +0100

    Add linux/platform.c

---
+++ linux/platform.c
@@ -0,0 +1,10 @@
+#include "core/platform.h"
+
+#include <time.h>
+
+// return monotonic time in microseconds
+uint32_t plat_time_usec(void) {
+	struct timespec time_mono;
+	clock_gettime(CLOCK_MONOTONIC, &time_mono);
+	return time_mono.tv_sec * 1000000 + time_mono.tv_nsec / 1000;
+}
