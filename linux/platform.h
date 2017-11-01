9fda1219ad78a05573d293be668ca546dd1c4d85
Bruno Randolf <br1@einfach.org>
2017-11-01 10:03:05 +0000

    Remove includes from linux/platform.h

--- linux/platform.h
+++ linux/platform.h
@@ -12,17 +12,5 @@
 #endif
 
 #include <stdint.h>
-#include <endian.h>
-
-// directly included:
-//#include <stdbool.h>	// bool
-//#include <stdint.h>	// int types
-//#include <stddef.h>	// size_t
-
-#include <string.h>	// memcpy
-#include <stdlib.h>	// malloc, free
-#include <math.h>	// pow
-#include <stdio.h>	// printf...
 
 uint32_t plat_time_usec(void);	// return monotonic time in usec
-
